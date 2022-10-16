93a08e9de1e5a25dda2bddbdf6d0e694aaf12bf4
Rohan-Swain <rohanswain2003@gmail.com>
2022-10-16 22:12:57 +0530

    Contact Page Layout: DONE

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Layout from "./Components/Layout";
 import Homepage from "./Components/Pages/Home";
 import Aboutpage from "./Components/Pages/About";
 import Projectspage from "./Components/Pages/Projects";
+import Contactpage from "./Components/Pages/Contact";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
           <Route index element={<Homepage />} />
           <Route path="/about" element={<Aboutpage />} />
           <Route path="/projects" element={<Projectspage />} />
-          <Route path="/contact" element={<Homepage />} />
+          <Route path="/contact" element={<Contactpage />} />
         </Route>
       </Routes>
     </>
---
+++ src/Components/Pages/Contact/index.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "./index.scss";
+import AnimatedLetter from "../../Animation";
+
+function Contactpage() {
+  const [letterClass, setLetterClass] = useState("letters");
+
+  setTimeout(() => setLetterClass("letters-hover"), 1500);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert("Done");
+  };
+
+  return (
+    <div className="container contactpage">
+      <div className="textarea">
+        <h1>
+          <AnimatedLetter
+            letterArr={"Contact".split("")}
+            letterClass={letterClass}
+            offset={1}
+          />
+          <AnimatedLetter
+            letterArr={"Me!".split("")}
+            letterClass={letterClass}
+            offset={8}
+            spanStyle={"space"}
+          />
+        </h1>
+        <p>For any Queries, feel free to contact me using the below form!</p>
+        <form onSubmit={handleSubmit}>
+          <div className="firstRow">
+            <input type={"text"} placeholder="Name" name="name" />
+            <input type={"email"} placeholder="E-mail" name="email" />
+          </div>
+          <div className="remainingFields">
+            <input type={"text"} placeholder="Subject" name="subject" />
+            <textarea placeholder="Message" />
+            <button type="submit">Send!</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Contactpage;
