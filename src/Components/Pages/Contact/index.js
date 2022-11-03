cc5513a95c673d502dbc2681f62fd03343338478
Rohan-Swain <rohanswain2003@gmail.com>
2022-11-03 17:10:05 +0530

    Contact page interaction: DONE

--- src/Components/Pages/Contact/index.js
+++ src/Components/Pages/Contact/index.js
@@ -4,12 +4,38 @@ import AnimatedLetter from "../../Animation";
 
 function Contactpage() {
   const [letterClass, setLetterClass] = useState("letters");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
 
   setTimeout(() => setLetterClass("letters-hover"), 1500);
 
+  const handleName = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleSubject = (event) => {
+    setSubject(event.target.value);
+  };
+
+  const handleMessage = (event) => {
+    setMessage(event.target.value);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert("Done");
+    const mail = document.createElement("a");
+    mail.href = `mailto:rohanswain2003@gmail.com?subject=${subject
+      .split(" ")
+      .join("%20")}&body=${name.split(" ").join("%20")}%0D%0D${email
+      .split("@")
+      .join("%40")}%0D%0D${message.split(" ").join("%20")}`;
+    mail.click();
   };
 
   return (
@@ -31,12 +57,27 @@ function Contactpage() {
         <p>For any Queries, feel free to contact me using the below form!</p>
         <form onSubmit={handleSubmit}>
           <div className="firstRow">
-            <input type={"text"} placeholder="Name" name="name" />
-            <input type={"email"} placeholder="E-mail" name="email" />
+            <input
+              type={"text"}
+              placeholder="Name"
+              name="name"
+              onChange={handleName}
+            />
+            <input
+              type={"email"}
+              placeholder="E-mail"
+              name="email"
+              onChange={handleEmail}
+            />
           </div>
           <div className="remainingFields">
-            <input type={"text"} placeholder="Subject" name="subject" />
-            <textarea placeholder="Message" />
+            <input
+              type={"text"}
+              placeholder="Subject"
+              name="subject"
+              onChange={handleSubject}
+            />
+            <textarea placeholder="Message" onChange={handleMessage} />
             <button type="submit">Send!</button>
           </div>
         </form>
