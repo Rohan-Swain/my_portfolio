46a5a6e21e10c9ec73759005bb35d74e583dddfa
Rohan-Swain <rohanswain2003@gmail.com>
2022-11-14 21:18:13 +0530

    removing GSAP library: DONE

--- src/Components/Pages/Contact/ContactLogo/index.js
+++ src/Components/Pages/Contact/ContactLogo/index.js
@@ -1,58 +1,18 @@
 import "./index.scss";
-import gsap from "gsap-trial";
-import { DrawSVGPlugin } from "gsap-trial/DrawSVGPlugin";
-import { useRef } from "react";
 import { useEffect } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAt } from "@fortawesome/free-solid-svg-icons";
 
 function ContactLogo() {
-  const messageBoxOutlineRef = useRef();
-  const messageBoxOutlineRef2 = useRef();
-
   useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin);
-
-    gsap
-      .timeline()
-      .fromTo(
-        messageBoxOutlineRef.current,
-        {
-          drawSVG: 0,
-        },
-        {
-          drawSVG: "100%",
-          duration: 1.5,
-          delay: 1.5,
-        }
-      )
-      .fromTo(
-        messageBoxOutlineRef2.current,
-        {
-          drawSVG: 0,
-        },
-        {
-          drawSVG: "100%",
-          duration: 1.5,
-          delay: 1.8,
-        }
-      );
-
-    dropShadow();
-  }, []);
-
-  const dropShadow = () => {
     setTimeout(
-      () => messageBoxOutlineRef.current.classList.add("dropShadow"),
+      () => document.querySelector(".messageBox1").classList.add("dropShadow"),
       3000
     );
 
     setTimeout(
-      () => messageBoxOutlineRef2.current.classList.add("dropShadow"),
-      6300
+      () => document.querySelector(".messageBox2").classList.add("dropShadow"),
+      6400
     );
-    return undefined;
-  };
+  }, []);
 
   return (
     <div id="contactLogo">
@@ -66,8 +26,7 @@ function ContactLogo() {
         <g id="layer1" transform="translate(-68.5,-60)">
           <g id="first-message-box">
             <path
-              ref={messageBoxOutlineRef}
-              className="whiteStroke"
+              className="whiteStroke messageBox1"
               id="message-box-outline1"
               d="m 88.660429,111.59358 28.937911,0.0459 c 2.58374,0 3.44246,-1.46691 3.44246,-3.66249 l 0.0727,-21.734616 c 0,-2.12361 -1.14995,-3.23428 -3.40249,-3.23428 l -44.590907,-0.0265 c -1.780546,0 -2.984476,1.13498 -2.984476,2.96845 l -0.0073,22.756776 c 0,1.55963 1.576675,2.92919 3.373934,2.92919 l 5.227241,-0.0286 c 1.032069,0 1.057128,0.004 0.947306,1.19716 l -0.294297,3.66863 c 0,0.51712 0.315761,0.59944 0.709063,0.37236 l 7.606326,-5.00709 c 0.369523,-0.23675 0.395342,-0.24491 0.962526,-0.24491 z"
             />
@@ -99,8 +58,7 @@ function ContactLogo() {
           </g>
           <g id="second-message-box">
             <path
-              ref={messageBoxOutlineRef2}
-              className="whiteStroke"
+              className="whiteStroke messageBox2"
               id="message-box-outline2"
               d="M 131.98544,76.04525 119.51115,76.082 c -2.06887,0 -2.75647,-1.174593 -2.75647,-2.93265 l -0.0582,-7.492975 c 0,-1.700428 0.92079,-2.58977 2.72446,-2.58977 l 25.00804,-0.02122 c 1.42573,0 2.38975,0.908808 2.38975,2.376914 l 0.006,8.311445 c 0,1.248835 -1.26249,2.345477 -2.7016,2.345477 l -4.18558,-0.0229 c -0.82641,0 -0.84647,0.0032 -0.75854,0.958597 l 0.23566,2.937566 c 0,0.414071 -0.25284,0.479987 -0.56777,0.298158 l -6.09058,-4.009305 c -0.29588,-0.189572 -0.31656,-0.196106 -0.77072,-0.196106 z"
             />
